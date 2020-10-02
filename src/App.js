a49963e1069ce5a8296a033cf4408f3f48673345
angelask <angela.strayhorn@yahoo.com>
2020-10-02 14:32:51 -0500

    updated app.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import logo from './logo.svg';
+
 import Main from "./components/Main";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
