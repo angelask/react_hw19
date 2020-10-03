75ddbf240198da948ce18b4439c96747fde8d919
angelask <angela.strayhorn@yahoo.com>
2020-10-02 20:24:14 -0500

    Updates

---
+++ precache-manifest.2f101796708b7a6a31716e65475a47e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b60540b0cd7406671e67b8dbce36e4b4",
+    "url": "/react_hw19/index.html"
+  },
+  {
+    "revision": "b0a2a2f02b66d22549fb",
+    "url": "/react_hw19/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "80ad5a41d74d0030bdaa",
+    "url": "/react_hw19/static/css/main.2f23ba9b.chunk.css"
+  },
+  {
+    "revision": "b0a2a2f02b66d22549fb",
+    "url": "/react_hw19/static/js/2.cecb5aae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_hw19/static/js/2.cecb5aae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80ad5a41d74d0030bdaa",
+    "url": "/react_hw19/static/js/main.6f9caaae.chunk.js"
+  },
+  {
+    "revision": "167790e5ea5a488236d8",
+    "url": "/react_hw19/static/js/runtime-main.a691f755.js"
+  }
+]);
